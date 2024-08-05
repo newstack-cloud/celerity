ddedf837d6a7059f16ed140ad3d19284a80b4fce
Andre Sutherland <andrelsf95@gmail.com>
2024-08-06 00:40:54 +0100

    wip(lib-rt-sdk-ffi-java): add java bindings for jni

---
+++ libs/runtime/sdk/bindgen-ffi-java/build.rs
@@ -0,0 +1,24 @@
+use std::path::{Path, PathBuf};
+
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=lib.rs");
+
+    // normally you'd never want to write files here, but this crate isn't used as a dependency
+    let out_path: PathBuf = Path::new(&std::env::var_os("OUT_DIR").unwrap()).join("jni.rs");
+
+    let config = oo_bindgen::backend::java::JniBindgenConfig {
+        group_id: "com.twohundred.celerity",
+        ffi_name: "celerity_runtime_bindgen_ffi",
+    };
+
+    match celerity_runtime_bindgen_schema::build_lib() {
+        Err(err) => {
+            eprintln!("{err}");
+            std::process::exit(-1);
+        }
+        Ok(lib) => {
+            oo_bindgen::backend::java::generate_jni(&out_path, &lib, &config).unwrap();
+        }
+    }
+}
