c596eab0a3d5a5839a0dfedb9108fbc2d542ed4a
Andre Sutherland <andrelsf95@gmail.com>
2024-08-06 00:45:45 +0100

    wip(runtime-bindings): add runtime bindings builder application

---
+++ libs/runtime/sdk/runtime-bindings/src/main.rs
@@ -0,0 +1,21 @@
+use std::path::Path;
+use std::rc::Rc;
+
+fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_target(false)
+        .init();
+
+    let builder_settings = oo_bindgen::cli::BindingBuilderSettings {
+        ffi_target_name: "celerity-runtime-bindgen-ffi",
+        jni_target_name: "celerity-runtime-bindgen-ffi-api",
+        ffi_name: "celerity_runtime_bindgen_ffi",
+        ffi_path: Path::new("sdk/bindgen-ffi").into(),
+        java_group_id: "com.twohundred",
+        destination_path: Path::new("sdk/bindings").into(),
+        library: Rc::new(celerity_runtime_bindgen_schema::build_lib().unwrap()),
+    };
+
+    oo_bindgen::cli::run(builder_settings);
+}
