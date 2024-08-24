ddae3d7310c14d072ef7b54c1359d0868ef30238
Andre Sutherland <andrelsf95@gmail.com>
2024-08-24 22:16:45 +0100

    feat(blueprint): add core functions for checking if string has prefix

---
+++ libs/blueprint/corefunctions/has_prefix.go
@@ -0,0 +1,78 @@
+package corefunctions
+
+import (
+	"context"
+	"strings"
+
+	"github.com/two-hundred/celerity/libs/blueprint/function"
+	"github.com/two-hundred/celerity/libs/blueprint/provider"
+)
+
+// HasPrefixFunction provides the implementation of
+// a function that checks if a string has a prefix.
+type HasPrefixFunction struct {
+	definition *function.Definition
+}
+
+// NewHasPrefixFunction creates a new instance of the HasPrefixFunction with
+// a complete function definition.
+func NewHasPrefixFunction() provider.Function {
+	return &HasPrefixFunction{
+		definition: &function.Definition{
+			Description: "Checks if a string starts with a given substring.",
+			FormattedDescription: "Checks if a string starts with a given substring.\n\n" +
+				"**Examples:**\n\n" +
+				"```\n${has_prefix(values.cacheClusterConfig.host, \"http://\")}\n```",
+			Parameters: []function.Parameter{
+				&function.ScalarParameter{
+					Type: &function.ValueTypeDefinitionScalar{
+						Label: "string",
+						Type:  function.ValueTypeString,
+					},
+					Description: "A valid string literal, reference or function call yielding a return value " +
+						"representing the string to check.",
+				},
+				&function.ScalarParameter{
+					Type: &function.ValueTypeDefinitionScalar{
+						Label: "string",
+						Type:  function.ValueTypeString,
+					},
+					Description: "The prefix to check for at the start of the string.",
+				},
+			},
+			Return: &function.ScalarReturn{
+				Type: &function.ValueTypeDefinitionScalar{
+					Label: "boolean",
+					Type:  function.ValueTypeBool,
+				},
+				Description: "True, if the string starts with the prefix, false otherwise.",
+			},
+		},
+	}
+}
+
+func (f *HasPrefixFunction) GetDefinition(
+	ctx context.Context,
+	input *provider.FunctionGetDefinitionInput,
+) (*provider.FunctionGetDefinitionOutput, error) {
+	return &provider.FunctionGetDefinitionOutput{
+		Definition: f.definition,
+	}, nil
+}
+
+func (f *HasPrefixFunction) Call(
+	ctx context.Context,
+	input *provider.FunctionCallInput,
+) (*provider.FunctionCallOutput, error) {
+	var inputStr string
+	var prefix string
+	if err := input.Arguments.GetMultipleVars(ctx, &inputStr, &prefix); err != nil {
+		return nil, err
+	}
+
+	hasPrefix := strings.HasPrefix(inputStr, prefix)
+
+	return &provider.FunctionCallOutput{
+		ResponseData: hasPrefix,
+	}, nil
+}
