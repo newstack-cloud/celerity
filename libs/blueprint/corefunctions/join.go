1fea4dfbf00ff4af12d1a9587f399fa47375afab
Andre Sutherland <andrelsf95@gmail.com>
2024-08-24 18:49:38 +0100

    feat(blueprint): add core function for join

---
+++ libs/blueprint/corefunctions/join.go
@@ -0,0 +1,79 @@
+package corefunctions
+
+import (
+	"context"
+	"strings"
+
+	"github.com/two-hundred/celerity/libs/blueprint/function"
+	"github.com/two-hundred/celerity/libs/blueprint/provider"
+)
+
+// JoinFunction provides the implementation of
+// a function that joins an array of strings into a single
+// string with a provided delimiter.
+type JoinFunction struct {
+	definition *function.Definition
+}
+
+// NewJoinFunction creates a new instance of the JoinFunction with
+// a complete function definition.
+func NewJoinFunction() provider.Function {
+	return &JoinFunction{
+		definition: &function.Definition{
+			Description: "Joins an array of strings into a single string using a delimiter.",
+			FormattedDescription: "Joins an array of strings into a single string using a delimiter.\n\n" +
+				"**Examples:**\n\n" +
+				"```\n${join(values.cacheClusterConfig.hosts, \",\")}\n```",
+			Parameters: []function.Parameter{
+				&function.ListParameter{
+					ElementType: &function.ValueTypeDefinitionScalar{
+						Label: "string",
+						Type:  function.ValueTypeString,
+					},
+					Description: "A reference or function call yielding a return value " +
+						"representing an array of strings to join together.",
+				},
+				&function.ScalarParameter{
+					Type: &function.ValueTypeDefinitionScalar{
+						Label: "string",
+						Type:  function.ValueTypeString,
+					},
+					Description: "The delimiter to join the strings with.",
+				},
+			},
+			Return: &function.ScalarReturn{
+				Type: &function.ValueTypeDefinitionScalar{
+					Label: "string",
+					Type:  function.ValueTypeString,
+				},
+				Description: "A single string that is the result of joining the array of strings with the delimiter.",
+			},
+		},
+	}
+}
+
+func (f *JoinFunction) GetDefinition(
+	ctx context.Context,
+	input *provider.FunctionGetDefinitionInput,
+) (*provider.FunctionGetDefinitionOutput, error) {
+	return &provider.FunctionGetDefinitionOutput{
+		Definition: f.definition,
+	}, nil
+}
+
+func (f *JoinFunction) Call(
+	ctx context.Context,
+	input *provider.FunctionCallInput,
+) (*provider.FunctionCallOutput, error) {
+	var inputStrSlice []string
+	var delimiter string
+	if err := input.Arguments.GetMultipleVars(ctx, &inputStrSlice, &delimiter); err != nil {
+		return nil, err
+	}
+
+	joined := strings.Join(inputStrSlice, delimiter)
+
+	return &provider.FunctionCallOutput{
+		ResponseData: joined,
+	}, nil
+}
---
+++ libs/blueprint/corefunctions/join_test.go
@@ -0,0 +1,84 @@
+package corefunctions
+
+import (
+	"context"
+
+	"github.com/two-hundred/celerity/libs/blueprint/function"
+	"github.com/two-hundred/celerity/libs/blueprint/provider"
+	. "gopkg.in/check.v1"
+)
+
+type JoinFunctionTestSuite struct {
+	callStack   function.Stack
+	callContext *functionCallContextMock
+}
+
+var _ = Suite(&JoinFunctionTestSuite{})
+
+func (s *JoinFunctionTestSuite) SetUpTest(c *C) {
+	s.callStack = function.NewStack()
+	s.callContext = &functionCallContextMock{
+		params: &blueprintParamsMock{},
+		registry: &functionRegistryMock{
+			functions: map[string]provider.Function{},
+			callStack: s.callStack,
+		},
+		callStack: s.callStack,
+	}
+}
+
+func (s *JoinFunctionTestSuite) Test_joins_strings_with_delimiter(c *C) {
+	splitFunc := NewJoinFunction()
+	s.callStack.Push(&function.Call{
+		FunctionName: "join",
+	})
+	output, err := splitFunc.Call(context.TODO(), &provider.FunctionCallInput{
+		Arguments: &functionCallArgsMock{
+			args: []any{
+				[]string{
+					"https://example1.com",
+					"https://example2.com",
+					"https://example3.com",
+					"https://example4.com",
+				},
+				",",
+			},
+			callCtx: s.callContext,
+		},
+		CallContext: s.callContext,
+	})
+
+	c.Assert(err, IsNil)
+	outputStr, isStr := output.ResponseData.(string)
+	c.Assert(isStr, Equals, true)
+	c.Assert(outputStr, Equals, "https://example1.com,https://example2.com,https://example3.com,https://example4.com")
+}
+
+func (s *JoinFunctionTestSuite) Test_returns_func_error_for_invalid_input(c *C) {
+	joinFunc := NewJoinFunction()
+	s.callStack.Push(&function.Call{
+		FunctionName: "join",
+	})
+	_, err := joinFunc.Call(context.TODO(), &provider.FunctionCallInput{
+		Arguments: &functionCallArgsMock{
+			args: []any{
+				// Expected a slice of strings.
+				"https://example.com,https://example2.com",
+				",",
+			},
+			callCtx: s.callContext,
+		},
+		CallContext: s.callContext,
+	})
+
+	c.Assert(err, NotNil)
+	funcErr, isFuncErr := err.(*function.FuncCallError)
+	c.Assert(isFuncErr, Equals, true)
+	c.Assert(funcErr.Message, Equals, "argument at index 0 is of type string, but target is of type slice")
+	c.Assert(funcErr.CallStack, DeepEquals, []*function.Call{
+		{
+			FunctionName: "join",
+		},
+	})
+	c.Assert(funcErr.Code, Equals, function.FuncCallErrorCodeInvalidArgumentType)
+}
