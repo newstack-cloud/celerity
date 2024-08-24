5c1faa6c2e45fef1367866ddba37b19c71f42037
Andre Sutherland <andrelsf95@gmail.com>
2024-08-24 17:04:33 +0100

    feat(blueprint): add core function for composable split_g

---
+++ libs/blueprint/corefunctions/split_g_test.go
@@ -0,0 +1,51 @@
+package corefunctions
+
+import (
+	"context"
+
+	"github.com/two-hundred/celerity/libs/blueprint/function"
+	"github.com/two-hundred/celerity/libs/blueprint/provider"
+	. "gopkg.in/check.v1"
+)
+
+type Split_G_FunctionTestSuite struct {
+	callStack   function.Stack
+	callContext *functionCallContextMock
+}
+
+var _ = Suite(&Split_G_FunctionTestSuite{})
+
+func (s *Split_G_FunctionTestSuite) SetUpTest(c *C) {
+	s.callStack = function.NewStack()
+	s.callContext = &functionCallContextMock{
+		params: &blueprintParamsMock{},
+		registry: &functionRegistryMock{
+			functions: map[string]provider.Function{},
+			callStack: s.callStack,
+		},
+		callStack: s.callStack,
+	}
+}
+
+func (s *Split_G_FunctionTestSuite) Test_returns_function_runtime_info_with_partial_args(c *C) {
+	split_G_Func := NewSplit_G_Function()
+	s.callStack.Push(&function.Call{
+		FunctionName: "split_g",
+	})
+	output, err := split_G_Func.Call(context.TODO(), &provider.FunctionCallInput{
+		Arguments: &functionCallArgsMock{
+			args: []any{
+				",",
+			},
+			callCtx: s.callContext,
+		},
+		CallContext: s.callContext,
+	})
+
+	c.Assert(err, IsNil)
+	c.Assert(output.FunctionInfo, DeepEquals, provider.FunctionRuntimeInfo{
+		FunctionName: "split",
+		PartialArgs:  []any{","},
+		ArgsOffset:   1,
+	})
+}
