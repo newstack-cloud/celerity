9e8373f8e41238d759c074d402f0ca0a1d96437f
Andre Sutherland <andrelsf95@gmail.com>
2024-08-24 13:26:54 +0100

    feat(blueprint): add core function for trimsuffix

---
+++ libs/blueprint/corefunctions/trimsuffix_test.go
@@ -0,0 +1,79 @@
+package corefunctions
+
+import (
+	"context"
+
+	"github.com/two-hundred/celerity/libs/blueprint/function"
+	"github.com/two-hundred/celerity/libs/blueprint/provider"
+	. "gopkg.in/check.v1"
+)
+
+type TrimSuffixFunctionTestSuite struct {
+	callStack   function.Stack
+	callContext *functionCallContextMock
+}
+
+var _ = Suite(&TrimSuffixFunctionTestSuite{})
+
+func (s *TrimSuffixFunctionTestSuite) SetUpTest(c *C) {
+	s.callStack = function.NewStack()
+	s.callContext = &functionCallContextMock{
+		params: &blueprintParamsMock{},
+		registry: &functionRegistryMock{
+			functions: map[string]provider.Function{},
+			callStack: s.callStack,
+		},
+		callStack: s.callStack,
+	}
+}
+
+func (s *TrimSuffixFunctionTestSuite) Test_trims_suffix(c *C) {
+	trimSuffixFunc := NewTrimSuffixFunction()
+	s.callStack.Push(&function.Call{
+		FunctionName: "trimsuffix",
+	})
+	output, err := trimSuffixFunc.Call(context.TODO(), &provider.FunctionCallInput{
+		Arguments: &functionCallArgsMock{
+			args: []any{
+				"https://example.com:3000",
+				":3000",
+			},
+			callCtx: s.callContext,
+		},
+		CallContext: s.callContext,
+	})
+
+	c.Assert(err, IsNil)
+	outputStr, isStr := output.ResponseData.(string)
+	c.Assert(isStr, Equals, true)
+	c.Assert(outputStr, Equals, "https://example.com")
+}
+
+func (s *TrimSuffixFunctionTestSuite) Test_returns_func_error_for_invalid_input(c *C) {
+	trimSuffixFunc := NewTrimSuffixFunction()
+	s.callStack.Push(&function.Call{
+		FunctionName: "trimsuffix",
+	})
+	_, err := trimSuffixFunc.Call(context.TODO(), &provider.FunctionCallInput{
+		Arguments: &functionCallArgsMock{
+			args: []any{
+				"https://example.com",
+				// A string is expected here, not an integer.
+				981073,
+			},
+			callCtx: s.callContext,
+		},
+		CallContext: s.callContext,
+	})
+
+	c.Assert(err, NotNil)
+	funcErr, isFuncErr := err.(*function.FuncCallError)
+	c.Assert(isFuncErr, Equals, true)
+	c.Assert(funcErr.Message, Equals, "argument at index 1 is of type int, but target is of type string")
+	c.Assert(funcErr.CallStack, DeepEquals, []*function.Call{
+		{
+			FunctionName: "trimsuffix",
+		},
+	})
+	c.Assert(funcErr.Code, Equals, function.FuncCallErrorCodeInvalidArgumentType)
+}
