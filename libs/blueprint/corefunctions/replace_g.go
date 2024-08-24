2ad411f5f73df813b44dd38bb20b798bb4d2bf46
Andre Sutherland <andrelsf95@gmail.com>
2024-08-24 11:23:13 +0100

    feat(blueprint): add core function for composable replace_g

---
+++ libs/blueprint/corefunctions/replace_g.go
@@ -0,0 +1,101 @@
+package corefunctions
+
+import (
+	"context"
+
+	"github.com/two-hundred/celerity/libs/blueprint/function"
+	"github.com/two-hundred/celerity/libs/blueprint/provider"
+)
+
+// Replace_G_Function provides the implementation of the
+// core "replace_g" function defined in the blueprint specification.
+type Replace_G_Function struct {
+	definition *function.Definition
+}
+
+// NewReplace_G_Function creates a new instance of the Replace_G_Function with
+// a complete function definition.
+func NewReplace_G_Function() provider.Function {
+	return &Replace_G_Function{
+		definition: &function.Definition{
+			Description: "A composable version of the \"replace\" function that takes the search and replace substrings " +
+				"as static arguments and returns a function that takes the string to replace the substrings in.",
+			FormattedDescription: "A composable version of the \"replace\" function that takes the search and replace substrings " +
+				"as static arguments and returns a function that takes the string to replace the substrings in.\n\n" +
+				"**Examples:**\n\n" +
+				"```\n${map(values.cacheClusterConfig.hosts, replace_g(\"http://\", \"https://\"))}\n```",
+			Parameters: []function.Parameter{
+				&function.ScalarParameter{
+					Type: &function.ValueTypeDefinitionScalar{
+						Label: "string",
+						Type:  function.ValueTypeString,
+					},
+					Description: "The \"search\" substring to replace.",
+				},
+				&function.ScalarParameter{
+					Type: &function.ValueTypeDefinitionScalar{
+						Label: "string",
+						Type:  function.ValueTypeString,
+					},
+					Description: "The substring to replace the \"search\" substring with.",
+				},
+			},
+			Return: &function.FunctionReturn{
+				FunctionType: &function.ValueTypeDefinitionFunction{
+					Definition: function.Definition{
+						Parameters: []function.Parameter{
+							&function.ScalarParameter{
+								Type: &function.ValueTypeDefinitionScalar{
+									Label: "string",
+									Type:  function.ValueTypeString,
+								},
+								Description: "A valid string literal, reference or function call yielding a return value " +
+									"representing an input string that contains a substring that needs replacing.",
+							},
+						},
+						Return: &function.ScalarReturn{
+							Type: &function.ValueTypeDefinitionScalar{
+								Label: "string",
+								Type:  function.ValueTypeString,
+							},
+							Description: "The input string with all occurrences of the \"search\" substring" +
+								" replaced with the \"replace\" substring.",
+						},
+					},
+				},
+				Description: "A function that takes a string and returns the substring from the string using " +
+					"the pre-configured \"search\" and \"replace\" strings.",
+			},
+		},
+	}
+}
+
+func (f *Replace_G_Function) GetDefinition(
+	ctx context.Context,
+	input *provider.FunctionGetDefinitionInput,
+) (*provider.FunctionGetDefinitionOutput, error) {
+	return &provider.FunctionGetDefinitionOutput{
+		Definition: f.definition,
+	}, nil
+}
+
+func (f *Replace_G_Function) Call(
+	ctx context.Context,
+	input *provider.FunctionCallInput,
+) (*provider.FunctionCallOutput, error) {
+	var search string
+	var replace string
+	err := input.Arguments.GetMultipleVars(ctx, &search, &replace)
+	if err != nil {
+		return nil, err
+	}
+
+	return &provider.FunctionCallOutput{
+		FunctionInfo: provider.FunctionRuntimeInfo{
+			FunctionName: "replace",
+			PartialArgs:  []interface{}{search, replace},
+			// The input string is passed as the first argument to the replace function.
+			ArgsOffset: 1,
+		},
+	}, nil
+}
---
+++ libs/blueprint/corefunctions/replace_g_test.go
@@ -0,0 +1,52 @@
+package corefunctions
+
+import (
+	"context"
+
+	"github.com/two-hundred/celerity/libs/blueprint/function"
+	"github.com/two-hundred/celerity/libs/blueprint/provider"
+	. "gopkg.in/check.v1"
+)
+
+type Replace_G_FunctionTestSuite struct {
+	callStack   function.Stack
+	callContext *functionCallContextMock
+}
+
+var _ = Suite(&Replace_G_FunctionTestSuite{})
+
+func (s *Replace_G_FunctionTestSuite) SetUpTest(c *C) {
+	s.callStack = function.NewStack()
+	s.callContext = &functionCallContextMock{
+		params: &blueprintParamsMock{},
+		registry: &functionRegistryMock{
+			functions: map[string]provider.Function{},
+			callStack: s.callStack,
+		},
+		callStack: s.callStack,
+	}
+}
+
+func (s *Replace_G_FunctionTestSuite) Test_returns_function_runtime_info_with_partial_args(c *C) {
+	Replace_G_Func := NewReplace_G_Function()
+	s.callStack.Push(&function.Call{
+		FunctionName: "replace_g",
+	})
+	output, err := Replace_G_Func.Call(context.TODO(), &provider.FunctionCallInput{
+		Arguments: &functionCallArgsMock{
+			args: []any{
+				"http://",
+				"https://",
+			},
+			callCtx: s.callContext,
+		},
+		CallContext: s.callContext,
+	})
+
+	c.Assert(err, IsNil)
+	c.Assert(output.FunctionInfo, DeepEquals, provider.FunctionRuntimeInfo{
+		FunctionName: "replace",
+		PartialArgs:  []any{"http://", "https://"},
+		ArgsOffset:   1,
+	})
+}
