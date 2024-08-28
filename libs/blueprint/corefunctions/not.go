82737e8672846c670b1046190b7c75a43cf5fdb1
Andre Sutherland <andrelsf95@gmail.com>
2024-08-28 16:31:46 +0100

    feat(blueprint): add not core function implementation

---
+++ libs/blueprint/corefunctions/not.go
@@ -0,0 +1,66 @@
+package corefunctions
+
+import (
+	"context"
+
+	"github.com/two-hundred/celerity/libs/blueprint/function"
+	"github.com/two-hundred/celerity/libs/blueprint/provider"
+)
+
+// NotFunction provides the implementation of
+// a function that acts as a logical NOT operator.
+type NotFunction struct {
+	definition *function.Definition
+}
+
+// NewNotFunction creates a new instance of the NotFunction with
+// a complete function definition.
+func NewNotFunction() provider.Function {
+	return &NotFunction{
+		definition: &function.Definition{
+			Description: "A function that negates a given boolean value.",
+			FormattedDescription: "A function that negates a given boolean value.\n\n" +
+				"**Examples:**\n\n" +
+				"```\n${not(eq(variables.environment, \"prod\"))}\n```",
+			Parameters: []function.Parameter{
+				&function.ScalarParameter{
+					Type: &function.ValueTypeDefinitionScalar{
+						Label: "boolean",
+						Type:  function.ValueTypeBool,
+					},
+					Description: "The result of a boolean expression to negate.",
+				},
+			},
+			Return: &function.ScalarReturn{
+				Type: &function.ValueTypeDefinitionScalar{
+					Label: "boolean",
+					Type:  function.ValueTypeBool,
+				},
+				Description: "The result of negating the provided boolean value.",
+			},
+		},
+	}
+}
+
+func (f *NotFunction) GetDefinition(
+	ctx context.Context,
+	input *provider.FunctionGetDefinitionInput,
+) (*provider.FunctionGetDefinitionOutput, error) {
+	return &provider.FunctionGetDefinitionOutput{
+		Definition: f.definition,
+	}, nil
+}
+
+func (f *NotFunction) Call(
+	ctx context.Context,
+	input *provider.FunctionCallInput,
+) (*provider.FunctionCallOutput, error) {
+	var toNegate bool
+	if err := input.Arguments.GetVar(ctx, 0, &toNegate); err != nil {
+		return nil, err
+	}
+
+	return &provider.FunctionCallOutput{
+		ResponseData: !toNegate,
+	}, nil
+}
