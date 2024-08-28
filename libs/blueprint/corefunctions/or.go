058da7e5353b6729903738151681dd2890373958
Andre Sutherland <andrelsf95@gmail.com>
2024-08-28 16:11:30 +0100

    feat(blueprint): add or core function implementation

---
+++ libs/blueprint/corefunctions/or.go
@@ -0,0 +1,74 @@
+package corefunctions
+
+import (
+	"context"
+
+	"github.com/two-hundred/celerity/libs/blueprint/function"
+	"github.com/two-hundred/celerity/libs/blueprint/provider"
+)
+
+// OrFunction provides the implementation of
+// a function that acts as a logical OR operator.
+type OrFunction struct {
+	definition *function.Definition
+}
+
+// NewOrFunction creates a new instance of the OrFunction with
+// a complete function definition.
+func NewOrFunction() provider.Function {
+	return &OrFunction{
+		definition: &function.Definition{
+			Description: "A function that acts as a logical OR operator on two boolean values.",
+			FormattedDescription: "A function that acts as a logical OR operator on two boolean values.\n\n" +
+				"**Examples:**\n\n" +
+				"```\n${or(resources.orderApi.state.isDev, eq(variables.environment, \"dev\"))}\n```",
+			Parameters: []function.Parameter{
+				&function.ScalarParameter{
+					Type: &function.ValueTypeDefinitionScalar{
+						Label: "boolean",
+						Type:  function.ValueTypeBool,
+					},
+					Description: "The result of boolean expression A, the left-hand side of the OR operation.",
+				},
+				&function.ScalarParameter{
+					Type: &function.ValueTypeDefinitionScalar{
+						Label: "boolean",
+						Type:  function.ValueTypeBool,
+					},
+					Description: "The result of boolean expression B, the right-hand side of the OR operation.",
+				},
+			},
+			Return: &function.ScalarReturn{
+				Type: &function.ValueTypeDefinitionScalar{
+					Label: "boolean",
+					Type:  function.ValueTypeBool,
+				},
+				Description: "The result of the logical OR operation on the two boolean values.",
+			},
+		},
+	}
+}
+
+func (f *OrFunction) GetDefinition(
+	ctx context.Context,
+	input *provider.FunctionGetDefinitionInput,
+) (*provider.FunctionGetDefinitionOutput, error) {
+	return &provider.FunctionGetDefinitionOutput{
+		Definition: f.definition,
+	}, nil
+}
+
+func (f *OrFunction) Call(
+	ctx context.Context,
+	input *provider.FunctionCallInput,
+) (*provider.FunctionCallOutput, error) {
+	var lhs bool
+	var rhs bool
+	if err := input.Arguments.GetMultipleVars(ctx, &lhs, &rhs); err != nil {
+		return nil, err
+	}
+
+	return &provider.FunctionCallOutput{
+		ResponseData: lhs || rhs,
+	}, nil
+}
---
+++ libs/blueprint/corefunctions/or_test.go
@@ -0,0 +1,79 @@
+package corefunctions
+
+import (
+	"context"
+
+	"github.com/two-hundred/celerity/libs/blueprint/function"
+	"github.com/two-hundred/celerity/libs/blueprint/provider"
+	. "gopkg.in/check.v1"
+)
+
+type OrFunctionTestSuite struct {
+	callStack   function.Stack
+	callContext *functionCallContextMock
+}
+
+var _ = Suite(&OrFunctionTestSuite{})
+
+func (s *OrFunctionTestSuite) SetUpTest(c *C) {
+	s.callStack = function.NewStack()
+	s.callContext = &functionCallContextMock{
+		params: &blueprintParamsMock{},
+		registry: &functionRegistryMock{
+			functions: map[string]provider.Function{},
+			callStack: s.callStack,
+		},
+		callStack: s.callStack,
+	}
+}
+
+func (s *OrFunctionTestSuite) Test_applies_logical_or(c *C) {
+	orFunc := NewOrFunction()
+	s.callStack.Push(&function.Call{
+		FunctionName: "or",
+	})
+	output, err := orFunc.Call(context.TODO(), &provider.FunctionCallInput{
+		Arguments: &functionCallArgsMock{
+			args: []any{
+				false,
+				true,
+			},
+			callCtx: s.callContext,
+		},
+		CallContext: s.callContext,
+	})
+
+	c.Assert(err, IsNil)
+	outputBool, isBool := output.ResponseData.(bool)
+	c.Assert(isBool, Equals, true)
+	c.Assert(outputBool, Equals, true)
+}
+
+func (s *OrFunctionTestSuite) Test_returns_func_error_for_invalid_input(c *C) {
+	orFunc := NewOrFunction()
+	s.callStack.Push(&function.Call{
+		FunctionName: "or",
+	})
+	_, err := orFunc.Call(context.TODO(), &provider.FunctionCallInput{
+		Arguments: &functionCallArgsMock{
+			args: []any{
+				true,
+				// A boolean is expected here, not an integer.
+				985043,
+			},
+			callCtx: s.callContext,
+		},
+		CallContext: s.callContext,
+	})
+
+	c.Assert(err, NotNil)
+	funcErr, isFuncErr := err.(*function.FuncCallError)
+	c.Assert(isFuncErr, Equals, true)
+	c.Assert(funcErr.Message, Equals, "argument at index 1 is of type int, but target is of type bool")
+	c.Assert(funcErr.CallStack, DeepEquals, []*function.Call{
+		{
+			FunctionName: "or",
+		},
+	})
+	c.Assert(funcErr.Code, Equals, function.FuncCallErrorCodeInvalidArgumentType)
+}
