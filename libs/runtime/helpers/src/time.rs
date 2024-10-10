deacf336edbaa45cf634c2f2d811292663465ab3
Andre Sutherland <andrelsf95@gmail.com>
2024-10-10 16:04:16 +0100

    feat(lib-rt-helpers): update clock to include current time in milliseconds

--- libs/runtime/helpers/src/time.rs
+++ libs/runtime/helpers/src/time.rs
@@ -1,9 +1,19 @@
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::{
+    fmt::Debug,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
 /// A trait for a clock that can provide the current time
 /// as a UNIX timestamp in seconds.
 pub trait Clock {
     fn now(&self) -> u64;
+    fn now_millis(&self) -> u64;
+}
+
+impl Debug for dyn Clock + Send + Sync {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "Clock")
+    }
 }
 
 /// A default implementation of a clock that uses the system time.
@@ -33,5 +43,12 @@ impl Clock for DefaultClock {
             .duration_since(UNIX_EPOCH)
             .expect("Time went backwards")
             .as_secs()
+    }
+
+    fn now_millis(&self) -> u64 {
+        SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_millis() as u64
     }
 }
