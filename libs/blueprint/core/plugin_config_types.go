dd9733eb06ad7224bd5ec5a7f199becb183f9424
Andre Sutherland <andrelsf95@gmail.com>
2025-04-13 13:45:09 +0100

    feat(blueprint): add secret to plugin config definitions

--- libs/blueprint/core/plugin_config_types.go
+++ libs/blueprint/core/plugin_config_types.go
@@ -9,9 +9,14 @@ type ConfigDefinition struct {
 // ConfigFieldDefinition represents a field in a configuration definition
 // for a provider or transformer plugin.
 type ConfigFieldDefinition struct {
-	Type          ScalarType     `json:"type"`
-	Label         string         `json:"label"`
-	Description   string         `json:"description"`
+	Type        ScalarType `json:"type"`
+	Label       string     `json:"label"`
+	Description string     `json:"description"`
+	// Set to true if the config field should be a secret,
+	// An application such as the deploy engine
+	// will ensure that the value of this field
+	// does not appear in logs.
+	Secret        bool           `json:"secret"`
 	DefaultValue  *ScalarValue   `json:"defaultValue,omitempty"`
 	AllowedValues []*ScalarValue `json:"allowedValues,omitempty"`
 	Examples      []*ScalarValue `json:"examples,omitempty"`
