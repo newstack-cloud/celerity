eee62578e5e6fe4ea53f10fc74aa9594725c74d8
Andre Sutherland <andrelsf95@gmail.com>
2025-06-09 02:28:59 +0100

    chore(plugin-docgen): update versions file to include latest version of the blueprint framework

--- tools/plugin-docgen/internal/env/versions.go
+++ tools/plugin-docgen/internal/env/versions.go
@@ -5,7 +5,7 @@ const (
 	// The version of the plugin docgen software.
 	pluginDocgenVersion = "dev"
 	// The version of the blueprint framework used by the plugin docgen tool.
-	blueprintFrameworkVersion = "v0.5.0"
+	blueprintFrameworkVersion = "v0.16.0"
 	// The version of the plugin framework used by the plugin docgen tool.
 	pluginFrameworkVersion = "v0.0.0-20250515000109-8d2746400999"
 )
