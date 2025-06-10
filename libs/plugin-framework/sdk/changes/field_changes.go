606db8d928a78cfb9b63d4f05b7c975695c54b5a
Andre Sutherland <andrelsf95@gmail.com>
2025-06-10 01:50:30 +0100

    feat(plugin-framework): add utils for working with field changes
    Adds util functions to convert slices of field changes
    to maps to allow for efficiently looking up field change
    values by field path in plugins when extracting changes
    as a part of deploying (create or update) a resource.

---
+++ libs/plugin-framework/sdk/changes/field_changes.go
@@ -0,0 +1,58 @@
+package changes
+
+import (
+	"github.com/two-hundred/celerity/libs/blueprint/core"
+	"github.com/two-hundred/celerity/libs/blueprint/provider"
+)
+
+// FieldChangesToNewValueMap converts a slice of FieldChange structs to a map
+// where the keys are the field paths and the values are pointers to the
+// corresponding new value for the field. This is useful for creating lookups
+// for potentially large sets of field changes, allowing for quick access
+// to new values by field paths.
+//
+// Multiple slices of field changes can be provided, as a convenience
+// when merging new and modified field changes into a single map.
+func FieldChangesToNewValueMap(
+	changes ...[]provider.FieldChange,
+) map[string]*core.MappingNode {
+	allChanges := []provider.FieldChange{}
+	for _, changeSlice := range changes {
+		allChanges = append(allChanges, changeSlice...)
+	}
+
+	fieldChangeMap := make(map[string]*core.MappingNode, len(allChanges))
+	for _, change := range allChanges {
+		if change.NewValue != nil {
+			fieldChangeMap[change.FieldPath] = change.NewValue
+		}
+	}
+
+	return fieldChangeMap
+}
+
+// FieldChangesToPrevValueMap converts a slice of FieldChange structs to a map
+// where the keys are the field paths and the values are pointers to the
+// corresponding previous value for the field. This is useful for creating lookups
+// for potentially large sets of field changes, allowing for quick access
+// to previous values by field paths.
+//
+// Multiple slices of field changes can be provided, as a convenience
+// when merging new and modified field changes into a single map.
+func FieldChangesToPrevValueMap(
+	changes ...[]provider.FieldChange,
+) map[string]*core.MappingNode {
+	allChanges := []provider.FieldChange{}
+	for _, changeSlice := range changes {
+		allChanges = append(allChanges, changeSlice...)
+	}
+
+	fieldChangeMap := make(map[string]*core.MappingNode, len(allChanges))
+	for _, change := range allChanges {
+		if change.PrevValue != nil {
+			fieldChangeMap[change.FieldPath] = change.PrevValue
+		}
+	}
+
+	return fieldChangeMap
+}
---
+++ libs/plugin-framework/sdk/changes/field_changes_test.go
@@ -0,0 +1,89 @@
+package changes
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"github.com/two-hundred/celerity/libs/blueprint/core"
+	"github.com/two-hundred/celerity/libs/blueprint/provider"
+)
+
+type FieldChangesSuite struct {
+	suite.Suite
+}
+
+func (s *FieldChangesSuite) Test_field_changes_to_new_value_map() {
+	modifiedFields := []provider.FieldChange{
+		{
+			FieldPath: "field1",
+			NewValue:  core.MappingNodeFromString("newValue1"),
+			PrevValue: core.MappingNodeFromString("oldValue1"),
+		},
+		{
+			FieldPath: "field2",
+			NewValue:  core.MappingNodeFromString("newValue2"),
+			PrevValue: core.MappingNodeFromString("oldValue2"),
+		},
+	}
+
+	newFields := []provider.FieldChange{
+		{
+			FieldPath: "field3",
+			NewValue:  core.MappingNodeFromString("newValue3"),
+		},
+		{
+			FieldPath: "field4",
+			NewValue:  core.MappingNodeFromString("newValue4"),
+		},
+	}
+
+	expected := map[string]*core.MappingNode{
+		"field1": modifiedFields[0].NewValue,
+		"field2": modifiedFields[1].NewValue,
+		"field3": newFields[0].NewValue,
+		"field4": newFields[1].NewValue,
+	}
+
+	result := FieldChangesToNewValueMap(modifiedFields, newFields)
+
+	s.Assert().Equal(expected, result)
+}
+
+func (s *FieldChangesSuite) Test_field_changes_to_prev_value_map() {
+	modifiedFields := []provider.FieldChange{
+		{
+			FieldPath: "field1",
+			NewValue:  core.MappingNodeFromString("newValue1"),
+			PrevValue: core.MappingNodeFromString("oldValue1"),
+		},
+		{
+			FieldPath: "field2",
+			NewValue:  core.MappingNodeFromString("newValue2"),
+			PrevValue: core.MappingNodeFromString("oldValue2"),
+		},
+	}
+
+	newFields := []provider.FieldChange{
+		{
+			FieldPath: "field3",
+			NewValue:  core.MappingNodeFromString("newValue3"),
+		},
+		{
+			FieldPath: "field4",
+			NewValue:  core.MappingNodeFromString("newValue4"),
+		},
+	}
+
+	expected := map[string]*core.MappingNode{
+		"field1": modifiedFields[0].PrevValue,
+		"field2": modifiedFields[1].PrevValue,
+	}
+
+	result := FieldChangesToPrevValueMap(modifiedFields, newFields)
+
+	s.Assert().Equal(expected, result)
+}
+
+func TestFieldChangesSuite(t *testing.T) {
+	suite.Run(t, new(FieldChangesSuite))
+}
